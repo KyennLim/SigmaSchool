9c78cdabbbda3512c20c337ec2ea0353beac129b
Lim Khai Yenn <Kyennlim@gmail.com>
2025-10-30 20:55:41 +0000

    made authentication system for login but need to fix auth and login

--- Login-app/src/App.jsx
+++ Login-app/src/App.jsx
@@ -1,18 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
+import RequireAuth from './components/RequireAuth.jsx'
+import { useState } from "react"
+import { AuthContext } from './AuthContext.js'
+import Dashboard from './pages/Dashboard.jsx'
 
-function App() {
 
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route element = {<Home />} path="/" />
-      <Route element = {<Login />} path="/Login" />
-    </Routes>
-    </BrowserRouter>
-      
-  )
+export default function App(){
+    const [token, setToken] = useState(null);
+
+    return (
+        <AuthContext.Provider value={{token,setToken}}>
+        <BrowserRouter>
+        <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Login />} path="/Login" />
+            <Route element={
+                <RequireAuth>
+                    <Dashboard/>
+                </RequireAuth>
+                } path="/" 
+                />
+        </Routes>
+        </BrowserRouter>
+        </AuthContext.Provider>
+    )
 }
-
-export default App
