73fbd29e092fabbe701145c0448a4ac39a43c597
Lim Khai Yenn <Kyennlim@gmail.com>
2025-11-11 21:34:10 +0000

    todo app done with a bug i can't fix

--- Todo-Appv3/src/pages/Home.jsx
+++ Todo-Appv3/src/pages/Home.jsx
@@ -1,9 +1,34 @@
-import { Container } from "react-bootstrap";
+import { useContext } from "react";
+import { TodoContext } from "../contexts/TodoContext";
+import { Container, Row, Col, Card, Badge } from "react-bootstrap";
 
-export default function Home() {
+
+export default function Home(){
+    const todos = useContext(TodoContext).todos;
     return (
-            <Container>
-                <h1 className="my-3">your todos</h1>
-            </Container>
-    );
+        <Container>
+            <h1 className="my-3">your todos</h1>
+            <Row>
+                <CardGroup todos={todos}/>
+            </Row>
+        </Container>
+    )
+}
+
+function CardGroup({todos}) {
+    return todos.map((todo) => {
+        const completed = todo.completed
+        const bg = completed ? "success" : "danger"
+        return (
+            <Col md={4} key={todo.id}>
+                <Card className="my-3">
+                    <Card.Body>
+                        <Card.Title>{todo.Title}</Card.Title>
+                        <Card.Text>{todo.Text}</Card.Text>
+                        <Badge bg={bg}>{!completed && "Not"}completed</Badge>
+                    </Card.Body>
+                </Card>
+            </Col>
+        )
+    }) 
 }
