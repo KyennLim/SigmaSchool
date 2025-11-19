d27260fc1fabb15e4ace30da68e1f7c67c26bb5e
Lim Khai Yenn <Kyennlim@gmail.com>
2025-11-19 19:45:43 +0000

    bro code header and footer made. Understanding of function increase and the use of elemnt

---
+++ Bro Code/sample_code_From_Bro_Code/src/Header.jsx
@@ -0,0 +1,19 @@
+
+
+function Header() {
+    return (
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                <li><a href=""/>Home</li>
+                <li><a href=""/>About</li>
+                <li><a href=""/>Service</li>
+                <li><a href=""/>Contact</li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+}
+export default Header
