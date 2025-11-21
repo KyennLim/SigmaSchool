29c2233e5b6d81c9b97276334cf4bc5333226be2
Lim Khai Yenn <Kyennlim@gmail.com>
2025-11-21 19:30:27 +0000

    learning different way to format style, external, modules and inline

---
+++ Bro Code/style-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Button from "./Button"
+import "./index.css"
+
+function App() {
+  return (
+    <Button/>
+  )
+}
+
+export default App
---
+++ Bro Code/style-project/src/Button.jsx
@@ -0,0 +1,16 @@
+function Button() {
+
+    const styles = {
+        backgroundColor: "rgb(72, 176, 245)",
+        color:"white",
+        padding:"10px 20px",
+        borderRadius: "5px",
+        border:" none",
+    }
+
+    return(
+        <button style={styles}>Click me</button>
+    )
+}
+
+export default Button
