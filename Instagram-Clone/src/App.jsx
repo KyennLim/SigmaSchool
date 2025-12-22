d6bc0566f65f758754bcec5442d51a8919858baf
Lim Khai Yenn <Kyennlim@gmail.com>
2025-12-22 19:15:28 +0000

    Button and side bar added part 1

--- Instagram-Clone/src/App.jsx
+++ Instagram-Clone/src/App.jsx
@@ -1,9 +1,17 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 
 export default function App() {
   return (
     <Row>
-      <Col sm={1}>Sidebar</Col>
+      <Col 
+      sm={1}
+      className="d-flex flex-column justify-content-start align0items-center vh-100 bg-light"
+      style={{ position: "sticky", top: 0 }}
+      >
+        <Button variant = {"light"} style={{ marginBottom: "7px" }}>
+          <i className={"bi bi-instagram"} style={{ "fontSize": "24px" }}></i>
+        </Button>
+      </Col>
       <Col sm={11}>Main</Col>
     </Row>
   )
