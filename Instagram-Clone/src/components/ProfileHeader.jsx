0ee2cc782be21f2fb64741b29fea9d998977c965
Lim Khai Yenn <Kyennlim@gmail.com>
2025-12-24 19:48:19 +0000

    profile image and profile description complete with custom dummy data

--- Instagram-Clone/src/components/ProfileHeader.jsx
+++ Instagram-Clone/src/components/ProfileHeader.jsx
@@ -1,10 +1,20 @@
-import { Row, Col, Image} from "react-bootstrap";
+import { Row, Col, Image, Button} from "react-bootstrap";
 import { useContext } from "react";
 import { ProfileContext } from "../App";
 
 
 function ProfileHeader(){
-    const { image } = useContext(ProfileContext);
+    const {
+        image, 
+        name, 
+        posts_no, 
+        followers, 
+        following, 
+        subheader,
+        account_type, 
+        description, 
+        link,
+    } = useContext(ProfileContext);
     return(
         <Row className="p-5">
             <Col md={3} className="d-flex align-items-center justify-content-center">
@@ -13,9 +23,28 @@ function ProfileHeader(){
                 alt="Profile"
                 className="rounded-circle"
                 style={{ width: "150px", height: "150px" }}
+                roundedCircle
                 />
             </Col>
-            <Col md={9}>profile description</Col>
+            <Col md={9}>
+            <span className="me-4" style={{ fontSize:"20px" }}>
+                {name}
+            </span>
+            <Button className="me-2">Follow back</Button>
+            <Button variant="light me-2">Message</Button>
+            <Button variant="light me-2">
+                <i className="bi bi-person-plus"></i>
+            </Button>
+            <Button variant="light">
+                <i className="bi bi-three-dots"></i>
+            </Button>
+            <br />
+            <br />
+            <p style={{ margin: 0, fontWeight: "bold"}}>{subheader}</p>
+            <p style={{ margin: 0, color: "grey"}}>{subheader}</p>
+            <p style={{ margin: 0 }}>{description}</p>
+            <a href={link}>{link}</a>
+            </Col>
         </Row>
     );
 }
