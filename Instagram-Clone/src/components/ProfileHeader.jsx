508452b9ce60b02a9216e509dc1a0763ca83df1e
Lim Khai Yenn <Kyennlim@gmail.com>
2025-12-24 19:38:07 +0000

    added dummy data

--- Instagram-Clone/src/components/ProfileHeader.jsx
+++ Instagram-Clone/src/components/ProfileHeader.jsx
@@ -1,12 +1,15 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Image} from "react-bootstrap";
+import { useContext } from "react";
+import { ProfileContext } from "../App";
 
 
 function ProfileHeader(){
+    const { image } = useContext(ProfileContext);
     return(
         <Row className="p-5">
             <Col md={3} className="d-flex align-items-center justify-content-center">
-                <img 
-                src="https://sig1.co/logo-1"
+                <Image 
+                src={image}
                 alt="Profile"
                 className="rounded-circle"
                 style={{ width: "150px", height: "150px" }}
