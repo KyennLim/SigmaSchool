972326875cfa7b9ff2ac12d0fc3fc9c9b3a4b849
Lim Khai Yenn <Kyennlim@gmail.com>
2026-01-16 19:24:03 +0000

    added 404 response html

--- RESTfullAPI/index.js
+++ RESTfullAPI/index.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Catch 404 and forward to error handler
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
