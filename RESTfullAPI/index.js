85860549a3d3c567e696605c4cd96cf96c5ce8f8
Lim Khai Yenn <Kyennlim@gmail.com>
2026-01-17 18:45:44 +0000

    connected to SQL server by URL somehow

--- RESTfullAPI/index.js
+++ RESTfullAPI/index.js
@@ -2,6 +2,29 @@ let express = require("express");
 let path = require("path");
 let app = express();
 
+const { Pool } = require("pg");
+require("dotenv").config();
+const { DATABSE_URL } = process.env;
+
+const pool = new Pool({
+  connectionString: DATABSE_URL,
+  ssl: {
+    require: truel,
+  },
+});
+
+async function getPostgresVersion() {
+  const client = await pool.connect();
+  try {
+    const response = await client.query("SELECT version()");
+    console.log(response.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPostgresVersion();
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
