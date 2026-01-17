3f3ac403737f194b5cc607e4263f63ebf6afb165
Lim Khai Yenn <Kyennlim@gmail.com>
2026-01-17 19:03:17 +0000

    trying to use DATABASE API and fix some bugs

--- RESTfullAPI/index.js
+++ RESTfullAPI/index.js
@@ -1,23 +1,46 @@
 let express = require("express");
 let path = require("path");
+const cors = require("cors"); // core is a middleware that allows us to make requests from one website to another website in the browser
 let app = express();
+app.use(cors());
+app.use(express.json());
 
 const { Pool } = require("pg");
 require("dotenv").config();
-const { DATABSE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 
 const pool = new Pool({
-  connectionString: DATABSE_URL,
+  connectionString: DATABASE_URL,
   ssl: {
-    require: truel,
+    require: true,
   },
 });
 
 async function getPostgresVersion() {
   const client = await pool.connect();
   try {
-    const response = await client.query("SELECT version()");
-    console.log(response.rows[0]);
+    const data = {
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      created_ai: new Date().toISOString,
+    };
+
+    const query =
+      "INSERT INTO posts (title, content, author, created_at) VALUES ($1, $2, $3, $4) RETURNING *";
+    const params = [data.title, data.content, data.author, data.created_at];
+
+    const result = await client.query(query, params);
+    date.id - result.row[0].id; // Add the ID to the data object
+
+    console.log(`Post created successfully with id ${data.id}`);
+    res.json({
+      status: "success",
+      data: data,
+      message: "Post created successfully",
+    });
+  } catch (error) {
+    console.error("Error: ", error.message);
   } finally {
     client.release();
   }
