917d6639e0670604a08fd4afaa1b76b1189fda27
Lim Khai Yenn <Kyennlim@gmail.com>
2026-02-02 19:03:11 +0000

    Login and signup page are setup frontend

--- signup_Login/src/components/loginForm.jsx
+++ signup_Login/src/components/loginForm.jsx
@@ -1,8 +1,34 @@
-
+import {Form, Button } from 'react-bootstrap';
+import {useState, useEffect} from 'react';
 
 function loginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        // Handle form submission logic here
+
+    };
     return (
-        <h2>loginForm</h2>
+        <div>
+            <h2>loginForm</h2>
+            <Form className="w-50 mx-auto" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
     );
 };
 
--- signup_Login/src/components/signupForm.jsx
+++ signup_Login/src/components/signupForm.jsx
@@ -1,8 +1,40 @@
-
+import {Form, Button } from 'react-bootstrap';
+import {useState, useEffect} from 'react';
 
 function signupForm() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+        // Handle form submission logic here
+
+        // remember to fetch db and match username so that they do not already exist
+
+
+    };
+
+    
     return (
-        <h2>signupForm</h2>
+        <div>
+            <h2>signupForm</h2>
+            <Form className="w-50 mx-auto" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
     );
 }
 
