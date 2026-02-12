e733bcbdbcbd3bc91d8c1a3b7fc1a1a2180b9a69
Lim Khai Yenn <Kyennlim@gmail.com>
2026-02-12 19:22:24 +0000

    login-signup ui

--- twitter_clone/src/pages/AuthPage.jsx
+++ twitter_clone/src/pages/AuthPage.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Image} from "react-bootstrap";
+import { Row, Col, Image, Button} from "react-bootstrap";
 
 
 export default function AuthPage() {
@@ -6,9 +6,33 @@ export default function AuthPage() {
     return (
         <Row>
             <Col sm={6}>
-            <Image src={loginImage} fluid />
+                <Image src={loginImage} fluid />
             </Col>
-            <Col sm={6}>Login</Col>
+            <Col sm={6} className="p-4">
+                <i className="bi bi-twitter" style={{ fontSize:50, color: "dodgerblue"}}></i>
+
+                <p className="mt-5" style={{ fontSize: 64 }}>Happening Now</p>
+                <h2 className="my-5" style={{ fontSize: 31 }}>Join Twitter Today</h2>
+
+                <Col sm={5} className="d-grid gap-2">
+                <Button className="rounded-pill" variant="outline-dark">
+                    <i className="bi bi-google"></i> Sign up with Google
+                </Button>
+                <Button className="rounded-pill" variant="outline-dark">
+                    <i className="bi bi-apple"></i> Sign up with Apple
+                </Button>
+                <p style={{ textAlign: "center"}}>or</p>
+                <Button className="rounded-pill">Create an account</Button>
+                <p style={{ fontSize: "12px"}}>
+                    By signing up, you agree to the Terms of Service and Privacy Policy including Cookie use.
+                </p>
+
+                <p className="mt-5" style={{ fontWeight: "bold" }}>
+                    Already have an account?
+                </p>
+                <Button className="rounded-pill" variant="outline-primary">Sign in</Button>
+                </Col>
+            </Col>
         </Row>
     );
 }
