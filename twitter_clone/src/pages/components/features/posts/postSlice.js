820ad4dc4d240b685940fd43f0c2aebdf740c0c6
Lim Khai Yenn <Kyennlim@gmail.com>
2026-02-27 19:31:17 +0000

    added jwt and slice to postslice post module

--- twitter_clone/src/pages/components/features/posts/postSlice.js
+++ twitter_clone/src/pages/components/features/posts/postSlice.js
@@ -1,18 +1,39 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import {jwtDecode} from "jwt-decode";
 
 const BASE_URL = "http://localhost:3000";
 
 // Async thunk for fetching a user's posts
 export const fetchPostsByUser = createAsyncThunk(
     "posts/fetchByUser",
-    async (userId, thunkAPI) => {
+    async (userId) => {
         const response = await fetch(`${BASE_URL}/posts/user/${userId}`);
         return response.json();
     }
 );
 
+// save post
+export const savePost = createAsyncThunk(
+    "posts/savePost",
+    async (postContent) => {
+        const token = localStorage.getItem("authToken");
+        const decode = jwtDecode(token);
+        const userid = decode.id;
+
+        const data = {
+            title: "Post Title", 
+            content: postContent,
+            user_id: userid
+        };
+
+        const response = await axios.post(`${BASE_URL}/posts`, data);
+        return response.data;
+    }
+);
+
 // Slice
-const postsSllice = createSlice({
+const postsSlice = createSlice({
     name: "posts",
     initialState: { posts: [], loading: true },
     reducers: {},
@@ -20,8 +41,11 @@ const postsSllice = createSlice({
         builder.addCase(fetchPostsByUser.fulfilled, (state, action) => {
             state.posts = action.payload;
             state.loading = false;
+        }),
+        builder.addCase(savePost.fulfilled, (state, action) => {
+            state.posts = [action.payload, ...state.posts]
         });
     }
 })
 
-export default postsSllice.reducer;
+export default postsSlice.reducer;
