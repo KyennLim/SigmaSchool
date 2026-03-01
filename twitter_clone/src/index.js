d407ba28565ecbf677c4a6f386c4db445b196c88
Lim Khai Yenn <Kyennlim@gmail.com>
2026-03-01 19:47:23 +0000

    revamp the query on GET likes/post/:id

--- twitter_clone/src/index.js
+++ twitter_clone/src/index.js
@@ -89,8 +89,11 @@ app.get("/likes/post/:post_id", async (req, res) => {
   const client = await pool.connect();
   try {
     const likes = await client.query(
-      "SELECT users.username, users.id AS user_id, likes.id AS likes_id FROM likes INNER JOIN users ON likes.user_id = users.id WHERE likes.post_id = $1",
-      [post_id],
+      `SELECT users.username, users.id AS user_id, likes.id AS likes_id
+       FROM likes
+       JOIN users ON likes.user_id = users.id
+       WHERE likes.post_id = $1
+       `,[post_id],
     );
     res.json(likes.rows);
   } catch (err) {
